5de9f6ab770b7813c8561188d6d03834ded8d1bb
ManNjoro <elijohnmwoho@gmail.com>
2025-07-23 01:30:39 +0300

    Paginate Issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,14 @@
 import { prisma } from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import { IssueStatusBadge, Link } from "@/app/components";
-import NextLink from 'next/link'
+import NextLink from "next/link";
 import IssueActions from "./IssueActions";
 import { Issue, Status } from "../generated/prisma";
 import { ArrowUpIcon } from "@radix-ui/react-icons";
+import Pagination from "../components/Pagination";
 
 interface Props {
-  searchParams: Promise<{ status: Status, orderBy: keyof Issue }>;
+  searchParams: Promise<{ status: Status; orderBy: keyof Issue; page: string }>;
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
@@ -17,17 +18,26 @@ const IssuesPage = async ({ searchParams }: Props) => {
     { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
   ];
   const statuses = Object.values(Status);
-  const sParams = (await searchParams)
-  const status = statuses.includes(sParams.status)
-    ? sParams.status
+  const sParams = await searchParams;
+  const status = statuses.includes(sParams.status) ? sParams.status : undefined;
+  const where = {status}
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(sParams.orderBy)
+    ? { [sParams.orderBy]: "asc" }
     : undefined;
 
-    const orderBy = columns.map(column => column.value).includes(sParams.orderBy)? { [sParams.orderBy]: 'asc'} : undefined
+    const page = parseInt(sParams.page) || 1
+    const pageSize = 10
 
   const issues = await prisma.issue.findMany({
-    where: { status },
-    orderBy
+    where,
+    orderBy,
+    skip: (page - 1) * pageSize,
+    take: pageSize
   });
+
+  const issueCount = await prisma.issue.count({ where})
   return (
     <div>
       <IssueActions />
@@ -35,11 +45,21 @@ const IssuesPage = async ({ searchParams }: Props) => {
         <Table.Header>
           <Table.Row>
             {columns.map((column) => (
-              <Table.ColumnHeaderCell key={column.value} className={column.className}>
-                <NextLink href={{
-                  query: { ...sParams, orderBy: column.value}
-                }}> {column.label}</NextLink>
-               {column.value === sParams.orderBy && < ArrowUpIcon className="inline" />}
+              <Table.ColumnHeaderCell
+                key={column.value}
+                className={column.className}
+              >
+                <NextLink
+                  href={{
+                    query: { ...sParams, orderBy: column.value },
+                  }}
+                >
+                  {" "}
+                  {column.label}
+                </NextLink>
+                {column.value === sParams.orderBy && (
+                  <ArrowUpIcon className="inline" />
+                )}
               </Table.ColumnHeaderCell>
             ))}
           </Table.Row>
@@ -63,6 +83,7 @@ const IssuesPage = async ({ searchParams }: Props) => {
           ))}
         </Table.Body>
       </Table.Root>
+      <Pagination currentPage={page} itemCount={issueCount} pageSize={pageSize} />
     </div>
   );
 };
